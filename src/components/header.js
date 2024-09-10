03d01a029b8827ed281e656e837f53e2fa9a561c
Davi Coelho <davicoelho.cont@gmail.com>
2024-09-10 12:41:12 -0300

    first main version done

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import logo from '../assets/logo.png';
+import './../styles/header.css';
 
 export default function Header() {
     return (
-        <header className="flex justify-between items-center mx-auto my-10 font-sans text-linkWhite">
+        <header className="flex justify-between items-center mx-auto mt-10 font-sans text-linkWhite">
             <img src={logo} alt="Logo agência Link" className="h-24 rounded-full" />
             <nav>
                 <ul className="flex justify-evenly space-x-2 text-lg">
                     <li><a href="#" className="p-2 px-4 transition duration-200 ease-in hover:bg-offWhite hover:font-medium hover:text-linkBlack rounded-xl">Home</a></li>
                     <li><a href="#" className="p-2 px-4 transition duration-200 ease-in hover:bg-offWhite hover:font-medium hover:text-linkBlack rounded-xl">Sobre nós</a></li>
-                    <li><a href="#" className="p-2 px-4 transition duration-200 ease-in hover:bg-offWhite hover:font-medium hover:text-linkBlack rounded-xl">Trabalho</a></li>
+                    <li><a href="#" className="p-2 px-4 transition duration-200 ease-in hover:bg-offWhite hover:font-medium hover:text-linkBlack rounded-xl">Serviços</a></li>
                     <li><a href="#" className="p-2 px-4 transition duration-200 ease-in hover:bg-offWhite hover:font-medium hover:text-linkBlack rounded-xl">Avaliações</a></li>
                 </ul>
             </nav>
--- src/components/mainText.js
+++ src/components/mainText.js
@@ -1,5 +1,21 @@
 import img from '../assets/code.jpg';
+import './../styles/mainText.css';
 
 export default function MainText() {
-    return;
+    return (
+        <main className="flex items-center justify-between w-4/5 mx-auto gap-60 text-linkWhite">
+            <div id="main" className="w-1/2">
+                <h3>Agência Link</h3>
+                <h1 className="text-6xl font-semibold">
+                    Impulsione os resultados da sua empresa com uma presença digital estratégica.
+                </h1>
+                <button id="botao1">Comece Agora</button>
+                <button id="botao2">Saiba Mais</button>
+            </div>
+
+            <div className="w-1/2 flex justify-center">
+                <img src={img} alt="Imagem principal do site" className="max-w-full h-auto rounded-3xl" />
+            </div>
+        </main>
+    );
 }
